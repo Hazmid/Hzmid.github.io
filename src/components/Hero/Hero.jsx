2fdd9c500044417e5f22044fc800d5e34a2789f4
Abdul Hamid Usman <Hazemidoo@gmail.com>
2021-01-28 11:38:26 +0100

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -25,9 +25,9 @@ const Header = () => {
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'} <span className="text-color-main">{name || 'Name'}</span>
+            {title || 'Hi, my name is'} <span className="text-color-main">{name || 'Haze'}</span>
             <br />
-            {subtitle || "I'm A MDFKN Developer."}
+            {subtitle || "I'm one helluva Developer."}
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
