71d57e95b8b84cd115aae70db735e589d64a8dbb
Abdul Hamid Usman <Hazemidoo@gmail.com>
2021-02-16 06:31:23 -0800

    2 to 1

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,9 +19,9 @@ export const heroData = {
 export const aboutData = {
   img: 'profilepic.jpg',
   paragraphOne:
-    'Hello there! Im Abdul Hamid Usman, Im a Front-end Developer and Web Developer from Abuja, learning the MERN stack and currently working as a Freelancer.',
+    'Hello there! Im Abdul Hamid Usman, Im a Front-end Developer and Web Developer from Abuja, learning the MERN stack and currently working as a Freelancer. Ive always had a knack for learning how things work. My career over the years has transitioned between Health, Art, Psychology, Fashion-Design etc. And then i found Technology and Product development.',
   paragraphTwo:
-    'Ive always had a knack for learning how things work. My career over the years has transitioned between Health, Art, Psychology, Fashion-Design etc. And then i found Technology and Product development. With 2 years working on the web i connect business requirements and Individual preferences with modern technology. Building and maintaining interactive, user-friendly and functional websites and apps. Blending the art of design with skill of programming for outstanding Front-end experience and web solutions. Building websites from start to finish with proactive feature optimization and utilizing modern frameworks and libraries. Im a firm believer in the mobile-first approach and im very passionate about aesthetics and UX/UI design.',
+    'Now with 2 years working on the web, i connect business requirements and Individual preferences with modern technology. Building and maintaining interactive, user-friendly and functional websites and apps. Blending the art of design with skill of programming for outstanding Front-end experience and web solutions. Building websites from start to finish with proactive feature optimization and utilizing modern frameworks and libraries. Im a firm believer in the mobile-first approach and im very passionate about aesthetics and UX/UI design.',
   paragraphThree:
     'My tech stack (for now) includes JavaScript, TypeScript, ReactJS, Redux, GatsbyJS, TailwindCSS, Bootstrap, Material UI, Sass, HTML, CSS, graphQL, jQuery, GIT, Github, Gsap, Animations, Vectors, wordPress, NPM, Webpack, E-commerce, SEO, Cross-browser compatibility, UX/UI design, Mobile-layout, Microsoft office suite.',
   resume: '', // if no resume, the button will not show up
