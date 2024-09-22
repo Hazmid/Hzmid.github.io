15898fccb23382044dd6b1e0e5f514c8f1a52419
Abdulhamid Usman <Hazemidoo@gmail.com>
2024-09-22 16:21:32 +0100

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -18,7 +18,7 @@ export const heroData = {
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'Profile24.jpg',
+  img: 'photograph.jpg',
   paragraphOne:
     "My name is Abdulhamid Usman, and I'm a Frontend Engineer with 4+ years of experience in software development working on websites and web applications with React.js , although I'm not biased to using andor learning to use other frameworks and libraries. I am very passionate about aesthetics and design.",
   paragraphTwo:
